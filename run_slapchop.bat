a59d5d9fae04a922f4ebcf0d2a45cbdd5ea3f460
= <=>
2024-04-15 02:54:49 +0000

    Fix run_slapchop.bat

--- run_slapchop.bat
+++ run_slapchop.bat
@@ -1,7 +1,8 @@
 #!/bin/bash
-touch out/slapchop-do-not-commit.py
-rm out/slapchop-do-not-commit.py
-touch out/slapchop-do-not-commit.py
+rm -rf out/
+mkdir out
+cp -r src/ out/
+
 set `cat TOKEN`
 while IFS= read -r line
 do
@@ -9,6 +10,6 @@ do
        *YOURTOKEN*) printf "%s\n" "${line/YOURTOKEN/$1}" ;;
        *) printf "%s\n" "$line" ;;
     esac
-done < src/slapchop.py > out/slapchop-do-not-commit.py
+done < src/slapchop.py > out/src/slapchop-do-not-commit.py
 
-python3 out/slapchop-do-not-commit.py
+python3 out/src/slapchop-do-not-commit.py
