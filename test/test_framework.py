28be4e836dd0211d390e8e63d1015b92fca208ca
= <=>
2024-04-15 19:32:24 +0000

    Improvements to test and test_framework: better logging with more flexibility, test_framework now supports more features.

--- test/test_framework.py
+++ test/test_framework.py
@@ -1,25 +1,46 @@
 import functools
 import traceback
+from enum import Enum
 
 class TestError(Exception):
     def __init__(self, message):
         self.message = message
+
+class LogMessageType(Enum):
+    SUCCEED = 1
+    FAILED = 2
+    INFO = 3
+
+def log(message, type=LogMessageType.INFO):
+    match type:
+        case LogMessageType.SUCCEED:
+            print("\033[1;32;40m SUCCESS: " + message)
+        case LogMessageType.FAILED:
+            print("\033[1;31;40m FAILED: " + message)
+        case LogMessageType.INFO:
+            print("\033[1;34;40m INFO: " + message)
 
 def test(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
         try:
             fn(*args, **kwargs)
-            print(f". PASSED: {fn.__name__}")
+            log(fn.__name__, LogMessageType.SUCCEED)
         except TestError as te:
-            print(f"FAILED: {fn.__name__} - {te.message}")
+            log(fn.__name__ + " - " + te.message, LogMessageType.FAILED)
         except:
-            print(f"FAILED: {fn.__name__} - Uncaught exception occurred. Traceback: ")
-            print(traceback.format_exc())
+            log(fn.__name__ + " - Uncaught exception occurred.", LogMessageType.FAILED)
+            log(traceback.format_exc())
     return wrapper
 
 def expect(left, right, on_fail):
     if left == right:
+        return True
+    else:
+        raise TestError(on_fail)
+
+def expect_none(item, on_fail):
+    if item is None:
         return True
     else:
         raise TestError(on_fail)
