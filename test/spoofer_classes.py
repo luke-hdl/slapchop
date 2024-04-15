03f652ca2aa11a4976ceb7072aeea8ccd021d798
= <=>
2024-04-15 16:36:18 +0000

    Basic spoofer classes. More restructure is needed to fit them.

---
+++ test/spoofer_classes.py
@@ -0,0 +1,34 @@
+import time
+
+class Client:
+    def __init__(self, user):
+        self.user = user
+
+class Message:
+    def __init__(self, author, channel, content, mentions):
+        self.author = author
+        self.channel = channel
+        self.content = content
+        self.mentions = mentions
+
+class RecordedMessage: #no discord equivalent; used to store the channel buffer
+    def __init__(self, message):
+        self.author = message.author
+        self.content = message.content
+        self.mentions = message.mentions
+        self.post_time = time.time()
+        
+class User:
+    def __init__(self, display_name, channel):
+        self.channel = channel
+        self.display_name = display_name
+        self.mention = "<@{}>".format(int(hash(display_name))) #this doesn't guarantee a unique mention, but that's more complicated; I'll account for it later
+
+class Channel:
+    def __init__(self, guild):
+        self.guild = guild
+        self.message_history = []
+
+class Guild:
+    def __init__(self):
+        pass #further details may be needed later: right now we only check if guild is None
